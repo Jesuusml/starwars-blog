746aee15bdab96ec9d85fbcddf4ca38c311aa8d1
Jesus ML <madafakingofficial@gmail.com>
2026-02-12 21:38:48 +0100

    Update Card component

--- react/src/components/Card.jsx
+++ react/src/components/Card.jsx
@@ -1,7 +1,9 @@
+import toTitleCase from "../utils/toTitleCase";
+
 export default function Card(props) {
   // Determines which prop was passed
   const [type, data] = Object.entries(props).find(
-    ([key]) => ['person', 'vehicle', 'planet'].includes(key)
+    ([key]) => ['character', 'vehicle', 'planet'].includes(key)
   ) || [];
 
   if (!type || !data) {
@@ -10,29 +12,43 @@ export default function Card(props) {
 
   // Only allowed keys are shown
   const allowedKeys = {
-    person: ['gender', 'hair_color', 'eye_color'],
+    character: ['gender', 'hair_color', 'eye_color'],
     vehicle: ['manufacturer', 'model', 'passengers'],
     planet: ['population', 'terrain', 'climate'],
   };
 
   return (
     <>
-      <h2>{data.name}</h2>
-      <ul>
-        {Object.entries(data)
-          .filter(([key]) => allowedKeys[type].includes(key))
-          .map(([key, value]) => (
-            <li key={key}>
-              <strong>{key}:</strong> {value.toString()}
-            </li>
-          ))}
-      </ul>
-      <button
-        className="btn btn-outline-primary"
-        onClick={props.detailsView}
-      >
-        Learn more!
-      </button>
+      <div className="card" style={{ width: "18rem" }}>
+        {/* <img className="card-img-top" src="/starwars-logo.png" alt="Card image cap" /> */}
+        <div className="card-body">
+          <h3 className="card-title mb-4">{data.name}</h3>
+          <ul>
+            {Object.entries(data)
+              .filter(([key]) => allowedKeys[type].includes(key))
+              .map(([key, value]) => (
+                <li key={key}>
+                  {toTitleCase(key)}: {value.toString()}
+                </li>
+              ))}
+          </ul>
+          <span className="d-flex">
+            <a
+              href="#"
+              className="btn btn-outline-primary me-auto"
+              onClick={props.detailsView}
+            >
+              Learn more!
+            </a>
+            <button
+              className="btn btn-outline-warning"
+              onClick={() => null}
+            >
+              <span>{"<3"}</span>
+            </button>
+          </span>
+        </div>
+      </div>
     </>
   );
 }

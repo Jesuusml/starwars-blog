afb3854f0e7d57588aa235d3c91cd44c74b1fca1
Jesus ML <madafakingofficial@gmail.com>
2026-02-12 21:23:59 +0100

    Add rows for Cards

--- react/src/pages/App.jsx
+++ react/src/pages/App.jsx
@@ -6,10 +6,39 @@ import Navbar from "../components/Navbar";
 import toTitleCase from "../utils/toTitleCase"
 
 export default function App() {
-  const [people, setPeople] = useState([]);
-  const [vehicles, setVehicles] = useState([]);
-  const [planets, setPlanets] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const luke = {
+    "created": "2026-02-10T23:01:12.206Z",
+    "edited": "2026-02-10T23:01:12.206Z",
+    "name": "Luke Skywalker",
+    "gender": "male",
+    "skin_color": "fair",
+    "hair_color": "blond",
+    "height": "172",
+    "eye_color": "blue",
+    "mass": "77",
+    "homeworld": "https://www.swapi.tech/api/planets/1",
+    "birth_year": "19BBY",
+    "vehicles": [
+      "https://www.swapi.tech/api/vehicles/14",
+      "https://www.swapi.tech/api/vehicles/30"
+    ],
+    "starships": [
+      "https://www.swapi.tech/api/starships/12",
+      "https://www.swapi.tech/api/starships/22"
+    ],
+    "films": [
+      "https://www.swapi.tech/api/films/1",
+      "https://www.swapi.tech/api/films/2",
+      "https://www.swapi.tech/api/films/3",
+      "https://www.swapi.tech/api/films/6"
+    ],
+    "url": "https://www.swapi.tech/api/people/1"
+  };
+  const arr = Array.from({ length: 7 }, () => ({ ...luke }));
+  const [people, setPeople] = useState(arr);
+  const [vehicles, setVehicles] = useState(arr);
+  const [planets, setPlanets] = useState(arr);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function getData() {
@@ -30,16 +59,16 @@ export default function App() {
   function simpleRendering() {
     return (
       <>
-        <h1>People</h1>
+        <h1>Characters</h1>
         {loading && (
           <h2>Loading data...</h2>
         )}
         {people ? (
-          people.map((person, index) => (
+          people.map((character, index) => (
             <Card
               key={index}
-              person={person}
-              detailsView={() => useNavigate(`/details/people/${person.uid}`)}
+              character={character}
+              detailsView={() => useNavigate(`/details/people/${character.uid}`)}
             />
           ))
         ) : `There is no data for people`}
@@ -78,7 +107,7 @@ export default function App() {
   // More complex rendering, escalable version
   function modularRendering() {
     const allData = [
-      { element: ['people', 'person'], content: people },
+      { element: ['characters', 'character'], content: people },
       { element: ['vehicles', 'vehicle'], content: vehicles },
       { element: ['planets', 'planet'], content: planets }
     ];
@@ -88,14 +117,16 @@ export default function App() {
         {allData.map(({ element, content }) => (
           <div key={element[0]}>
             <h1>{toTitleCase(element[0])}</h1>
-            {loading && (
-              <h2>Loading data...</h2>
-            )}
-            {content ? (
-              content.map((value, index) => (
-                <Card key={index} {...{ [element[1]]: value }} />
-              ))
-            ) : `There is no data for ${element[0]}`}
+            <div className="cards-row d-flex overflow-auto gap-3 p-4 mb-5">
+              {loading && (
+                <h2>Loading data...</h2>
+              )}
+              {content ? (
+                content.map((value, index) => (
+                  <Card key={index} {...{ [element[1]]: value }} />
+                ))
+              ) : `There is no data for ${element[0]}`}
+            </div>
           </div>
         ))}
       </>
@@ -107,7 +138,7 @@ export default function App() {
     <>
       <Navbar />
       <div id="main" className="container">
-        {simpleRendering()}
+        {modularRendering()}
       </div>
     </>
   );

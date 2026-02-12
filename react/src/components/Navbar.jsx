b7b999fbdc48bed0f43379a07eafca56e4716858
Jesus ML <madafakingofficial@gmail.com>
2026-02-12 21:38:48 +0100

    Add navbar.jsx to /components

---
+++ react/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg px-2 mb-4">
+      <a className="navbar-brand px-2" href="#">
+        <img src="/starwars-logo.png" alt="StarWars Logo" />
+      </a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ms-auto me-2">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Favorites
+            </a>
+            <div className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="#">Action</a>
+              <a className="dropdown-item" href="#">Another action</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}

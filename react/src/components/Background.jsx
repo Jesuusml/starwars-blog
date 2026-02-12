26b5a1420b1f977b91ee206377fb32880e2cfa23
Jesus ML <madafakingofficial@gmail.com>
2026-02-12 21:38:48 +0100

    FIX: Adjust variables, refactor division with calc() function instead

--- react/src/components/Background.jsx
+++ react/src/components/Background.jsx
@@ -1,7 +1,7 @@
 export default function Background() {
   return (
     <div className="stars-layer">
-      {Array.from({ length: 10 }).map((_, i) => (
+      {Array.from({ length: 15 }).map((_, i) => (
         <div className={`layer${i + 1}`} key={i}></div>
       ))}
     </div>
